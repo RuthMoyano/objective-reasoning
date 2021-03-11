ff13d91ade2ff2426136732592db07ed39f9c838
Ruth Moyano <ruth.moyano@codeimmersives.com>
2021-03-11 11:42:28 -0600

    [add changes]

--- main.js
+++ main.js
@@ -2,8 +2,47 @@
 // │ Your code here! │	
 // └─────────────────┘
 
+function isAdmin (user) {
+    if (user.userRole === 'ADMIN') {
+        return true;
+    } else {
+        return false;
+    }
+}
 
+function getEmail (user) {
+    const lowerCaseLetterFirst = user.firstName.toLowerCase();
+    const lowerCaseLetterLast = user.lastName.toLowerCase();
+    return `${lowerCaseLetterFirst}.${lowerCaseLetterLast}@codeimmersives.com`;
+}
 
+function getPlaylistLength (object) {
+    return object.songs.length;
+}
+
+function getHardestHomework (assignment) {
+    console.log(assignment);
+    let first = Infinity;
+    let second = '';
+    for (const item of assignment) {
+        if (item.averageScore < first) {
+            second = item.name;
+            first = item.averageScore;
+        }
+    }
+    return second;
+}
+
+function createPhonebook (names, nums) {
+    const phonebook = {};
+    for (let i = 0; i < names.length; i++) {
+        const name = names[i];
+        const num = nums[i];
+        phonebook[name] = num;
+    }
+
+    return phonebook;
+}
 // ┌─────────────────────────────────────┐
 // │ Do not modify code below this line. │
 // └─────────────────────────────────────┘
